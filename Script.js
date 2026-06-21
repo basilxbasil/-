7433c584c2e97cfcdadcfe70cc70a92ae425ef60
basilxbasil <basilxbasil@gmail.com>
2026-06-21 05:15:10 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,6 @@
-console.log("الموقع يعمل بنجاح!");
+console.log("تم تشغيل تطبيق 'دورها' بنجاح!");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const app = document.getElementById("app");
+    console.log("الهيكل الأساسي جاهز للعمل.");
+});
