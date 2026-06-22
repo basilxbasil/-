d3cc75e7f9763c28d454f1c93725d30315b7da7e
basilxbasil <basilxbasil@gmail.com>
2026-06-22 14:10:09 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,33 +1,35 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "sonner";
-import { AuthProvider } from "@/contexts/AuthContext";
-import Home from "@/pages/Home";
-import ListingDetail from "@/pages/ListingDetail";
-import CreateListing from "@/pages/CreateListing";
-import Chats from "@/pages/Chats";
-import ChatDetail from "@/pages/ChatDetail";
-import Profile from "@/pages/Profile";
-import Search from "@/pages/Search";
-import Login from "@/pages/Login";
-import Register from "@/pages/Register";
+import { AuthProvider } from "./contexts/AuthContext";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Search from "./pages/Search";
+import CreateListing from "./pages/CreateListing";
+import ListingDetail from "./pages/ListingDetail";
+import Chats from "./pages/Chats";
+import ChatRoom from "./pages/ChatRoom";
+import Profile from "./pages/Profile";
+import Admin from "./pages/Admin";
 
 export default function App() {
   return (
     <AuthProvider>
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/listing/:id" element={<ListingDetail />} />
-          <Route path="/create" element={<CreateListing />} />
-          <Route path="/chats" element={<Chats />} />
-          <Route path="/chats/:id" element={<ChatDetail />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/search" element={<Search />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/create" element={<CreateListing />} />
+          <Route path="/listing/:id" element={<ListingDetail />} />
+          <Route path="/chats" element={<Chats />} />
+          <Route path="/chats/:convId" element={<ChatRoom />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/admin" element={<Admin />} />
         </Routes>
-        <Toaster position="top-center" />
-      </Router>
+        <Toaster position="top-center" richColors closeButton />
+      </BrowserRouter>
     </AuthProvider>
   );
 }
