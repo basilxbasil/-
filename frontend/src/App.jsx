d91ab52bd731b87d18e6b7467edeea44095d0324
basilxbasil <basilxbasil@gmail.com>
2026-06-22 15:05:23 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "sonner";
-import { AuthProvider } from "@/contexts/AuthContext";
-import Home from "@/pages/Home";
-import Login from "@/pages/Login";
-import Register from "@/pages/Register";
-import Search from "@/pages/Search";
-import CreateListing from "@/pages/CreateListing";
-import ListingDetail from "@/pages/ListingDetail";
-import Chats from "@/pages/Chats";
-import ChatRoom from "@/pages/ChatRoom";
-import Profile from "@/pages/Profile";
-import Admin from "@/pages/Admin";
+import { AuthProvider } from "./contexts/AuthContext";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Search from "./pages/Search";
+import CreateListing from "./pages/CreateListing";
+import ListingDetail from "./pages/ListingDetail";
+import Chats from "./pages/Chats";
+import ChatRoom from "./pages/ChatRoom";
+import Profile from "./pages/Profile";
+import Admin from "./pages/Admin";
 
 export default function App() {
   return (
@@ -28,7 +28,6 @@ export default function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/admin" element={<Admin />} />
         </Routes>
-        <Toaster position="top-center" richColors closeButton />
       </BrowserRouter>
     </AuthProvider>
   );
